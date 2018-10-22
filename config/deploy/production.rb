d232dc4d5b2efe2d44797306fa609bd04bf5e9e4
Tu Liang <info@tuliang.org>
2018-10-22 17:58:29 +0800

    add capistrano stage

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -6,6 +6,8 @@
 # server "example.com", user: "deploy", roles: %w{app db web}, my_property: :my_value
 # server "example.com", user: "deploy", roles: %w{app web}, other_property: :other_value
 # server "db.example.com", user: "deploy", roles: %w{db}
+set :stage, :production
+
 server "47.92.214.225", user: "deploy", roles: %w{app db web}, my_property: :my_value
 
 set :rails_env, :production
