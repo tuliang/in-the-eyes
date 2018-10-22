2158d39ec95873a93a1a2319736de0e9abe1b364
Tu Liang <info@tuliang.org>
2018-10-23 05:25:22 +0800

    fix install_docker

--- config/deploy.rb
+++ config/deploy.rb
@@ -127,7 +127,10 @@ namespace :deploy do
   end
 
   def install_docker
-  	sudo "curl -sSL https://git.io/install-docker | bash"
+  	# sudo "curl -sSL https://git.io/install-docker | bash"
+    sudo 'curl -L "https://github.com/docker/compose/releases/download/1.22.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose'
+    sudo "chmod +x /usr/local/bin/docker-compose"
+    sudo "docker-compose --version"
   end
 
   def install_db
