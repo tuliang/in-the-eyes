15c4977774cc8fc800038c2468b86bb658cce342
Tu Liang <info@tuliang.org>
2018-10-24 02:42:18 +0800

    fix build-image

--- config/deploy.rb
+++ config/deploy.rb
@@ -44,20 +44,9 @@ namespace :deploy do
   task :update do
     on roles(:web), in: :sequence, wait: 3 do
       within release_path do
-      	execute "cd #{release_path}"
+        execute "cd '#{release_path}/build_image/in-the-eyes'; git checkout master; git pull"
 
-        # execute "mkdir build_image"
-        execute "cd build_image/in-the-eyes"
-        # execute "git clone https://github.com/tuliang/in-the-eyes.git"
-        execute "git checkout master"
-        execute "git pull"
-
-
-        sudo "sh scripts/build-image"
-        # sudo "curl -sSL https://raw.githubusercontent.com/tuliang/in-the-eyes/master/scripts/build-image | bash"
-
-        # sudo "docker build -f"
-        # sudo "docker-compose pull"
+        sudo "curl -sSL https://raw.githubusercontent.com/tuliang/in-the-eyes/master/scripts/build-image | bash"
 
         restart_app
       end
