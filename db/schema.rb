3884b736601e93a42ee5f3343b314f7ef263eb32
Tu Liang <info@tuliang.org>
2018-10-24 12:37:01 +0800

    images add user_id

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_10_23_042735) do
+ActiveRecord::Schema.define(version: 2018_10_24_043606) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,6 +20,7 @@ ActiveRecord::Schema.define(version: 2018_10_23_042735) do
     t.integer "image_type"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id"
   end
 
 end
