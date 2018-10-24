629aca313012163bc10588cd92df8fad530f4268
Tu Liang <info@tuliang.org>
2018-10-24 16:55:43 +0800

    images controller add authenticate user

--- test/controllers/images_controller_test.rb
+++ test/controllers/images_controller_test.rb
@@ -7,42 +7,36 @@ class ImagesControllerTest < ActionDispatch::IntegrationTest
 
   test "should get index" do
     get images_url
-    assert_response :success
+    assert_redirected_to new_user_session_path
   end
 
   test "should get new" do
     get new_image_url
-    assert_response :success
+    assert_redirected_to new_user_session_path
   end
 
   test "should create image" do
-    assert_difference('Image.count') do
-      post images_url, params: { image: { image_type: @image.image_type, title: @image.title } }
-    end
-
-    assert_redirected_to image_url(Image.last)
+    post images_url, params: { image: { image_type: @image.image_type, title: @image.title } }
+    assert_redirected_to new_user_session_path
   end
 
   test "should show image" do
     get image_url(@image)
-    assert_response :success
+    assert_redirected_to new_user_session_path
   end
 
   test "should get edit" do
     get edit_image_url(@image)
-    assert_response :success
+    assert_redirected_to new_user_session_path
   end
 
   test "should update image" do
     patch image_url(@image), params: { image: { image_type: @image.image_type, title: @image.title } }
-    assert_redirected_to image_url(@image)
+    assert_redirected_to new_user_session_path
   end
 
   test "should destroy image" do
-    assert_difference('Image.count', -1) do
-      delete image_url(@image)
-    end
-
-    assert_redirected_to images_url
+    delete image_url(@image)
+    assert_redirected_to new_user_session_path
   end
 end
