9c600085890a87527db7a0ab87800ca45215bdd8
Tu Liang <info@tuliang.org>
2018-10-26 18:51:10 +0800

    fix image show

--- app/controllers/images_controller.rb
+++ app/controllers/images_controller.rb
@@ -1,5 +1,5 @@
 class ImagesController < ApplicationController
-  before_action :authenticate_user!
+  before_action :authenticate_user!, except: [:show]
   before_action :set_image, only: [:show, :edit, :update, :destroy]
   before_action :check_user, only: [:edit, :update, :destroy]
 
