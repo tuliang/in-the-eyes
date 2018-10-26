d4b3c7fbc98c66833ff7cb323fc6affaa519c8fe
Tu Liang <info@tuliang.org>
2018-10-26 17:35:53 +0800

    fix db seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,7 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-user_id = 2
+user_id = 1
 
 Image.delete_all
 
