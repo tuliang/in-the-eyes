3126317558905d6e38c7c260cd5cae41ef43f21f
Tu Liang <info@tuliang.org>
2018-11-10 17:11:19 +0800

    fix image comment cable

--- Gemfile
+++ Gemfile
@@ -8,6 +8,9 @@ gem 'rails', '~> 5.2.1'
 # Use sqlite3 as the database for Active Record
 # gem 'sqlite3'
 gem "pg"
+
+gem "redis"
+
 # Use Puma as the app server
 gem 'puma', '~> 3.11'
 # Use SCSS for stylesheets
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -45,7 +45,7 @@ Rails.application.configure do
   # config.action_cable.mount_path = nil
   # config.action_cable.url = 'wss://example.com/cable'
   # config.action_cable.allowed_request_origins = [ 'http://example.com', /http:\/\/example.*/ ]
-  config.action_cable.allowed_request_origins = [ 'http://47.92.214.225' ]
+  # config.action_cable.allowed_request_origins = [ 'http://47.92.214.225' ]
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
   # config.force_ssl = true
